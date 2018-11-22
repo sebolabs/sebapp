7b6a745a69b7a4d6df93a6eeec8ceacdbd4969db
Sebastian Mincewicz <s.mincewicz@kainos.com>
2018-11-22 11:44:52 +0000

    test release

---
+++ sebapp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  sebapp
+//
+//  Created by SEB on 23/10/2018.
+//  Copyright © 2018 SeboLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Seb was here :P", message: "Click Here", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
